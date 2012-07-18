e190aa1e55209ee99d4776c9948308002f323a43
Ben Durbin <ben@tabsandspaces.com>
2012-07-18 11:50:53 -0700

    Switching to use of Drupal behaviors for onPageLoad behavior. Adding warnings when switching a node with local translations to use Lingotek.

--- js/lingotek.form.js
+++ js/lingotek.form.js
@@ -1,5 +1,9 @@
+var lingotek = lingotek || {};
+lingotek.forms = lingotek.forms || {};
+
 (function ($) {
-  $(document).ready(function() {
+
+  lingotek.forms.init = function() {
     $("#edit-language")
       .change(function() {
         var language = $("#edit-language").val();
@@ -18,6 +22,7 @@
         }
       })
       .change();
+
     $("#edit-mtengine")
       .change(function() {
         var engine = $("#edit-mtengine").val();
@@ -29,5 +34,21 @@
         }
       })
       .change();
-  });
+
+    $('#edit-create-lingotek-document').change(function() {
+      if (Drupal.settings.lingotek_no_document &&
+        Drupal.settings.lingotek_has_entity_translations &&
+        $(this).val() == 1 &&
+        !lingotek.forms.new_document_alert_shown) {
+
+        alert(Drupal.t('Warning: This node has @translation_count local translations. These translations will be removed when sending this node to Lingotek for the first time. If this is not your intention, please set the \'Push Node to Lingotek\' field to \'No\' before saving this form.', {'@translation_count': Drupal.settings.lingotek_has_entity_translations}));
+        lingotek.forms.new_document_alert_shown = true;
+      }
+    });
+  };
+
 })(jQuery);
+
+Drupal.behaviors.lingotekSetupStatus = {
+  attach: lingotek.forms.init
+}
