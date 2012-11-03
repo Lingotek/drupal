15804e792d3b9c7e8e1fc652efb4d8e2273a3868
Ben Durbin <ben@tabsandspaces.com>
2012-11-02 17:01:47 -0700

    Removing client-side machine translation options. MT is now done via Workflows on Lingotek servers.

--- js/lingotek.form.js
+++ js/lingotek.form.js
@@ -3,34 +3,20 @@ lingotek.forms = lingotek.forms || {};
 
 (function ($) {
 
+  // Page setup for node add/edit forms.
   lingotek.forms.init = function() {
     $("#edit-language")
       .change(function() {
         var language = $("#edit-language").val();
         var sourceLanguageSet = language != 'und';
         $('#lingotek_fieldset').drupalSetSummary(Drupal.t(sourceLanguageSet ? 'Enabled' : 'Disabled'));
-        //Show all languages for MT Translation
-        $('#lingotek_fieldset #edit-mttargets > div').show();
         if (sourceLanguageSet) {
           $('#edit-note').hide();
           $('#edit-content').show();
-          $('#lingotek_fieldset .form-item-mtTargets-' + language).hide();
         }
         else {
           $('#edit-note').show();
           $('#edit-content').hide();
-        }
-      })
-      .change();
-
-    $("#edit-mtengine")
-      .change(function() {
-        var engine = $("#edit-mtengine").val();
-        if (engine == '0') {
-          $("#lingotek_fieldset .form-item-mtTargets").hide();
-        }
-        else {
-          $("#lingotek_fieldset .form-item-mtTargets").show();
         }
       })
       .change();
