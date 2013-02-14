5612240e31d14851ec9f98c2ef31961cad951b62
Matt Smith <msmith@lingotek.com>
2013-02-14 14:58:37 -0700

    minor clean-up

--- lib/Drupal/lingotek/LingotekApi.php
+++ lib/Drupal/lingotek/LingotekApi.php
@@ -69,9 +69,9 @@ class LingotekApi {
     global $_lingotek_locale;
     $success = TRUE;
 
-    $project_id = (!empty($node->lingotek_project_id)) ? $node->lingotek_project_id : NULL;
-    $project_id = $project_id !== NULL ? $project_id : lingotek_lingonode($node->nid, 'project_id');
-    $project_id = $project_id !== FALSE ? $project_id : variable_get('lingotek_project', NULL);
+    $project_id = empty($node->lingotek_project_id) ? NULL : $node->lingotek_project_id;
+    $project_id = empty($project_id) ? lingotek_lingonode($node->nid, 'project_id') : $project_id;
+    $project_id = empty($project_id) ? variable_get('lingotek_project', NULL) : $project_id;
 
     $source_language = ( isset( $_lingotek_locale[$node->language] ) ) ? $_lingotek_locale[$node->language] : $_lingotek_locale[variable_get( 'lingotek_source_language' )];
 
