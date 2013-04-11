27d35eea86163c011057187be54de9de6952568c
Matt Smith <msmith@lingotek.com>
2013-04-11 11:08:27 -0600

    bulk sync, incomplete translations option available

---
+++ lib/Drupal/lingotek/LingotekLog.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @file
+ * LingotekLog
+ */
+
+/**
+ * Wrapper logging class for watchdog
+ */
+class LingotekLog {
+  /*
+    WATCHDOG_EMERGENCY: Emergency, system is unusable.
+    WATCHDOG_ALERT: Alert, action must be taken immediately.
+    WATCHDOG_CRITICAL: Critical conditions.
+    WATCHDOG_ERROR: Error conditions.
+    WATCHDOG_WARNING: Warning conditions.
+    WATCHDOG_NOTICE: (default) Normal but significant conditions.
+    WATCHDOG_INFO: Informational messages.
+    WATCHDOG_DEBUG: Debug-level messages.
+   */
+  private static function getDefault(){
+    return (LINGOTEK_DEV == TRUE);
+  }
+
+  public static function info($msg, $data, $tag = '') {
+    self::log($msg, $data, $depth = 1, WATCHDOG_INFO, $tag);
+  }
+
+  public static function error($msg, $data, $tag = '') {
+    self::log($msg, $data, $depth = 1, WATCHDOG_ERROR, $tag);
+  }
+
+  public static function warning($msg, $data, $tag = 'warning') {
+    self::log($msg, $data, $depth = 1, WATCHDOG_WARNING, $tag);
+  }
+
+  public static function trace($msg, $data = NULL, $tag = 'trace') {
+    if (!variable_get('lingotek_trace_log', self::getDefault())) {
+      return;
+    }
+    self::log($msg, $data, $depth = 1, WATCHDOG_INFO, $tag);
+  }
+
+  public static function format($obj) {
+    return '<pre>' . htmlspecialchars(var_export($obj, TRUE)) . '</pre>';
+  }
+
+  private static function log($msg, $data = NULL, $depth = 0, $severity = WATCHDOG_NOTICE, $tag = '') {
+    if ($severity == WATCHDOG_WARNING && variable_get('lingotek_warning_log', self::getDefault())) {
+      return;
+    }
+    else if (strcasecmp($tag, 'api') == 0 && !variable_get('lingotek_api_debug', self::getDefault())) {
+      return;
+    }
+
+    $backtrace = debug_backtrace();
+    $location = $backtrace[$depth]['file'] . ':' . $backtrace[$depth]['line'];
+    $function = $backtrace[$depth + 1]['function'];
+    $args = @json_encode($backtrace[$depth + 1]['args']);
+
+    $data_output = "";
+    if (isset($data)) {
+      $data_output = json_encode($data);
+    }
+    $suffix = is_string($tag) && strlen($tag) ? ' - ' . $tag : '';
+    watchdog
+        (
+        'lingotek' . $suffix, t($msg, is_array($data) ? $data : array()) . ' <div style="word-break: break-all; padding-top: 10px; color: #666;"><b>MESSAGE:</b> %msg <br /><b>DATA:</b> %data <br /><b>FILE:</b> %location<br /><b>FUNCTION:</b> %function<br /><b>ARGS:</b> %args</div>', array(
+      '%msg' => $msg,
+      '%data' => $data_output,
+      '%location' => $location,
+      '%function' => $function,
+      '%args' => $args,
+        ), $severity
+    );
+
+    if (variable_get('lingotek_error_log', FALSE)) {
+      error_log("MESSAGE: $msg DATA: $data_output FILE: $location FUNCTION: $function ARGS: $args");
+    }
+  }
+
+}
+
+?>
