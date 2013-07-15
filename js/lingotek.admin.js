85f7610677fdd5253bd3bc8931416d6923e26293
Thomas Hansen <thansen@lingotek.com>
2013-07-15 16:57:05 -0600

    by thomas4019,smithworx,sngoodman: UI overhaul of the lingotek settings page. Minor changes to the lingotek setup system.

---
+++ js/lingotek.admin.js
@@ -0,0 +1,63 @@
+/**
+ * @file
+ * Custom javascript.
+ */
+
+(function ($) {
+
+Drupal.behaviors.lingotekAdminForm = {
+  attach: function (context) {
+
+    $('fieldset.lingotek-account', context).drupalSetSummary(function (context) {
+      return Drupal.t($('#connection_summary').val() + '<br>' + $('#account_summary').val());
+    });
+    
+//    $('fieldset.lingotek-connection-status', context).drupalSetSummary(function (context) {
+//      return Drupal.t();
+//    });
+    
+    $('fieldset.lingotek-translate-content', context).drupalSetSummary(function (context) {
+      $list = [];
+      $('#edit-node-translation input').each(function( index ) {
+        if($(this).attr('id').substring(0, 9) == 'edit-type' && $(this).attr('checked') == '1') {
+          $list.push($(this).val());
+        }
+      });
+      return Drupal.t($list.join(', '));
+    });
+    
+    $('fieldset.lingotek-translate-comments', context).drupalSetSummary(function (context) {
+      $list = [];
+      $('#edit-lingotek-translate-comments-node-types input').each(function( index ) {
+        if($(this).attr('checked') == '1') {
+          $list.push($(this).val());
+        }
+      });
+      return Drupal.t($list.join(', '));
+    });
+
+    $('fieldset.lingotek-translate-configuration', context).drupalSetSummary(function (context) {
+      $list = [];
+      $('#edit-additional-translation input').each(function( index ) {
+        if($(this).attr('checked') == '1') {
+          name = $(this).attr('name');
+          name = name.substring(name.lastIndexOf('_') + 1, name.length);
+          $list.push(name);
+        }
+      });
+      return Drupal.t($list.join(', '));
+    });
+    
+    $('fieldset.lingotek-preferences', context).drupalSetSummary(function (context) {
+      $list = [];
+      $('#edit-region').each(function( index ) {
+        if($(this).attr('checked') == '1') {
+          $list.push($(this).val());
+        }
+      });
+      return Drupal.t($list.join(', '));
+    });
+  }
+};
+
+})(jQuery);
