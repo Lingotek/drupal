fd245e7d7290883c828614a7753ce806cacf228f
Thomas Hansen <thansen@lingotek.com>
2013-08-20 16:37:50 -0600

    by thomas4019: Changes the UI of the Bulk grid to use popups.

--- js/lingotek.bulk_grid.js
+++ js/lingotek.bulk_grid.js
@@ -4,6 +4,21 @@
  */
 
 (function ($) {
+  function lingotekUpdateActionVisibilty() {
+    var count = 0;
+
+    $('#edit-grid-container .form-checkbox').each(function() {
+      var isChecked = $(this).attr('checked');
+      count += isChecked ? 1 : 0;
+    });
+
+    if (count > 0) {
+      $('.form-item-actions-select').show();
+    } else {
+      $('.form-item-actions-select').hide();
+    }
+  }
+  
   Drupal.behaviors.lingotekBulkGrid = {
     attach: function (context) {
       $('input#edit-submit-changes.form-submit').hide();
@@ -21,6 +36,23 @@
         width: 1200,
         modal: true
       });*/
+      
+      $('.form-item-actions-select').hide();
+      
+      
+      $('.select-all').change(function() {
+        lingotekUpdateActionVisibilty();
+      });
+      
+            
+      $('input#edit-actions-submit.form-submit').hide();
+      $('#edit-actions-select').change(function() {
+        $('input#edit-actions-submit.form-submit').trigger('click');
+      });
+      
+      $('#edit-grid-container .form-checkbox').change(function() {
+        lingotekUpdateActionVisibilty();
+      });
   }
 };
 
