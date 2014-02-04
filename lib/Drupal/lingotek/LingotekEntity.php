5330d399b59a178b08333d883ae77e873b1e3e4d
Matt Smith <msmith@lingotek.com>
2014-02-04 16:40:37 -0700

    Issue by bisle: fix uploaded url so that it is not unduly encoded

--- lib/Drupal/lingotek/LingotekEntity.php
+++ lib/Drupal/lingotek/LingotekEntity.php
@@ -311,9 +311,10 @@ class LingotekEntity implements LingotekTranslatableEntity {
   }
   
   public function getUrl() {
+    global $base_url;
     if ($this->entity_type == 'node' || $this->entity_type == 'comment') {
       $hack = (object) array('language' => ''); // this causes the url function to not prefix the url with the current language the user is viewing the site in
-      return url("lingotek/view/" . $this->getEntityType() . '/' . $this->getId() . '/{locale}', array('absolute' => TRUE, 'alias' => TRUE, 'language' => $hack));
+      return $base_url . "/lingotek/view/" . $this->getEntityType() . '/' . $this->getId() . '/{locale}';
     }
     return '';
   }
