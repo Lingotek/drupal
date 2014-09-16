6ac05b6ca8edba9328a577703a4187cc8b5c3cc2
Rob Bailey <rbailey@lingotek.com>
2014-09-16 15:24:04 -0600

    Added dynamic reporting to dashboard page.

--- src/Controller/LingotekDashboardController.php
+++ src/Controller/LingotekDashboardController.php
@@ -3,7 +3,9 @@
 namespace Drupal\lingotek\Controller;
 
 use Drupal\lingotek\Controller\LingotekControllerBase;
+use Drupal\lingotek\LingotekLocale;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Returns responses for lingotek module setup routes.
@@ -25,8 +27,41 @@ class LingotekDashboardController extends LingotekControllerBase {
     return $string;
   }
 
+  public function dashboardStatsPage(Request $request) {
+    $stats_array = array(
+    'method' => 'GET',
+    'count' => 6,
+    );
+    // Get languages
+    $languages = \Drupal::languageManager()->getLanguages();
+    $active_languages = 0;
+    $enabled_languages = 0;
+
+    foreach (array_keys($languages) as $langcode) {
+      $active_languages++; // WTD: pull from active languages
+      $enabled_languages++; // WTD: pull from lingotek-enabled languages
+      $locale = LingotekLocale::convertDrupal2Lingotek($langcode);
+
+      $stats_array['languages'][$locale] = $this->getLanguageReport($langcode);
+    }
+    // FIXME: make source and target totals dynamic
+    $stats_array['source'] = array(
+      'types' => array(
+        'node' => 0,
+      ),
+      'total' => 0,
+    );
+    $stats_array['target'] = array(
+      'types' => array(
+        'node' => 0,
+      ),
+      'total' => 0,
+    );
+    return JsonResponse::create($stats_array);
+  }
+
   protected function getDashboardInfo() {
-    global $base_url;
+    global $base_url, $base_root;
     return array(
       "community_id"=> $this->L->get('default.community'),
       "external_id"=> $this->L->get('account.login_id'),
@@ -45,7 +80,36 @@ class LingotekDashboardController extends LingotekControllerBase {
       "cms_tag"=> 'CMS TAG HERE',
       "locale"=> "en_US", // FIX: should be currently selected locale
       "module_version" => '1.x',
-      "endpoint_url" => '',
+      "endpoint_url" => $base_root . \Drupal::url('lingotek.dashboard_stats'),
     );
   }
+
+  protected function getLanguageReport($langcode, $active = 1, $enabled = 1) {
+    $locale = LingotekLocale::convertDrupal2Lingotek($langcode);
+    $stat = array(
+      'locale' => $locale,
+      'xcode' => $langcode,
+      'active' => 1,
+      'enabled' => 1,
+      'source' => array(
+        'types' => array(),
+        'total' => 0,
+      ),
+      'target' => array(
+        'types' => array(),
+        'total' => 0,
+      ),
+    );
+    foreach ($this->getEnabledTypes() as $type) {
+      $stat['source']['types'][$type] = 0;
+      $stat['target']['types'][$type] = 0;
+    }
+    return $stat;
+  }
+
+  protected function getEnabledTypes() {
+    // WTD: get the types enabled for lingotek translation
+    return array('node');
+  }
+
 }
