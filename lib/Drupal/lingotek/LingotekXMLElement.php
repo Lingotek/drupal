0938ebbc8fbfd10f8521ef071c5635efe53cd76f
Rob Bailey <rbailey@lingotek.com>
2015-04-09 15:37:40 -0600

    Issue #487: changed xml-building logic on entity upload to use LingotekXMLElement instead of manually constructing XML elements.

--- lib/Drupal/lingotek/LingotekXMLElement.php
+++ lib/Drupal/lingotek/LingotekXMLElement.php
@@ -14,4 +14,21 @@ class LingotekXMLElement extends SimpleXMLElement {
     $doc = $xml->ownerDocument;
     $xml->appendChild($doc->createCDATASection($text));
   }
+
+  /**
+   * Add a SimpleXMLElement object as a child
+   * @param SimpleXMLElement $child
+   * @param type $element_name
+   */
+  public function addChildXML(SimpleXMLElement $child, $element_name = NULL) {
+    if (!$element_name) {
+      $element_name = $child->getName();
+    }
+    $this_child = $this->addChild($element_name);
+    $to_dom = dom_import_simplexml($this_child);
+    $from_dom = dom_import_simplexml($child);
+    foreach ($from_dom->childNodes as $child_node) {
+      $to_dom->appendChild($to_dom->ownerDocument->importNode($child_node, TRUE));
+    }
+  }
 }
