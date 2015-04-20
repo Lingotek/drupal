1da38cddaa168788dc4afc71a8b6ff4234cf1db4
Rob Bailey <rbailey@lingotek.com>
2015-04-20 13:53:14 -0600

    Issue by robertdbailey: fixed indentation in LingotekConfigForm class.

--- src/Form/LingotekConfigFormBase.php
+++ src/Form/LingotekConfigFormBase.php
@@ -17,26 +17,31 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 abstract class LingotekConfigFormBase extends ConfigFormBase {
 
-	/**
-	 * Constructs a \Drupal\lingotek\Form\LingotekConfigFormBase object.
-	 *
+  /**
+   * Constructs a \Drupal\lingotek\Form\LingotekConfigFormBase object.
+   *
    * @param \Drupal\lingotek\LingotekInterface $lingotek
-	 * @param \Drupal\Core\Config\ConfigFactory $config
-	 *   The factory for configuration objects.
-	 */
-	public function __construct(LingotekInterface $lingotek, ConfigFactoryInterface $config) {
-		$this->L = $lingotek;
+   * @param \Drupal\Core\Config\ConfigFactory $config
+   *   The factory for configuration objects.
+   */
+  public function __construct(LingotekInterface $lingotek, ConfigFactoryInterface $config) {
+    $this->L = $lingotek;
     parent::__construct($config);
-	}
+  }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public static function create(ContainerInterface $container) {
-		return new static (
-			$container->get('lingotek'),
-			$container->get('config.factory')
-		);
-	}
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static (
+      $container->get('lingotek'),
+      $container->get('config.factory')
+    );
+  }
+
+  public function getEditableConfigNames() {
+    return [
+    ];
+  }
 
 }
