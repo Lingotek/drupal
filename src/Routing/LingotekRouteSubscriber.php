d43b67d881fd0b8e0de11d83499b821c50db8f23
Joseph Hovik <jhovik@lingotek.com>
2015-04-30 15:55:06 -0600

    Revert "Issue by robertdbailey: additional route changes for compatibility with beta9."
    This reverts commit 21f3c690710b0ff8a6da65765b43b178d20fd9fc.

---
+++ src/Routing/LingotekRouteSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\lingotek\Routing\LingotekRouteSubscriber.
+ */
+
+namespace Drupal\lingotek\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Subscriber to alter entity translation routes.
+ */
+class LingotekRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Use instead of ContentTranslationController.
+    foreach ($collection as $route) {
+      if ($route->getDefault('_controller') == '\Drupal\content_translation\Controller\ContentTranslationController::overview') {
+        dpm($route->getDefault('_controller'), 'old');
+        $route->setDefault('_controller', '\Drupal\lingotek\Controller\LingotekContentTranslationController::overview');
+        dpm($route->getDefault('_controller'), 'new');
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = parent::getSubscribedEvents();
+    //  ContentTranslationRouteSubscriber is -100.
+    $events[RoutingEvents::ALTER] = array('onAlterRoutes', -220);
+    return $events;
+  }
+
+}
