63cb85744e0f729f528a600616b44aef192494d5
sethwhite <swhite@lingotek.com>
2015-05-14 12:36:03 -0600

    Checkboxes/filtering now affects upload/download buttons. Added legend to manage tab of translation page.

--- js/lingotek.bulk_grid.js
+++ js/lingotek.bulk_grid.js
@@ -41,6 +41,8 @@ function lingotek_perform_action(nid, action) {
 
   Drupal.behaviors.lingotekBulkGrid = {
     attach: function (context) {
+      addClickToDownloadReady();
+      addClickToUploadButton();
       $('.form-checkbox').change(function() {
         var cells_of_selected_row = $(this).parents("tr").children();
 
@@ -88,5 +90,108 @@ function lingotek_perform_action(nid, action) {
       });
   }
 };
-
+ //the whole concept behind this function is kinda hacky, I'm pulling the data
+    //straight from the html instead of getting data from the backend, I considered
+    //pulling from the database and filtering in the PHP, but there's no guarantee
+    //the results would match the frontend's filters
+  function scrapeAllFilteredPages(element_id, original_URL) {
+    var entity_ids = getIDArray(true);//gets the displayed data
+    var requestCount = 0;
+    //cancel the default link href, otherwise it tries to follow the link before
+    //the ajax calls finish
+    $(element_id).removeAttr('href');
+    //get the data for every page option on the table
+    $('.pager li.pager-item a').each(function(){
+     var href = $(this).attr('href');
+      $.ajax({
+          url: href,
+          dataType: 'text',
+          success: function(data) {
+            //here we're grabbing the inputs inside the table
+            var begin = data.indexOf("<table");
+            var end = data.indexOf("</table>") + "</table>".length;
+            var tableData = data.substring(begin, end);
+            var el = $('<div></div>');
+            el.html(tableData);
+            $('input', el).each(function(){
+                entity_ids.push($(this).val());
+            });    
+          },
+          complete: function(){
+              requestCount++;
+              //after the last page is processed, execute the new URL with a POST
+              //request
+              if(requestCount === $('.pager li.pager-item a').length){
+                clickFilteredURL(entity_ids, original_URL, element_id);
+              }
+          }
+      });       
+    });
+    //covers the case of only one page of results
+    if($('.pager li.pager-item a').length === 0 && entity_ids.length > 0){
+        clickFilteredURL(entity_ids, original_URL, element_id);
+    }
+    
+  }
+  function clickFilteredURL(entity_ids, original_URL){
+    //the entity_type is passed as a URL param
+    //the entities to upload/download are sent in a POST body to deal with URL 
+    //length restraints
+    var new_URL = original_URL.valueOf();
+    new_URL += entity_ids.length !== 0 ? "/" + entity_ids.join(",") : "";
+      var form = $('<form>', {
+        'action': original_URL + "?render=overlay",
+        'method': "post"
+      }).append($('<input>', {
+        'name': 'comma_separated_ids',
+        'value': entity_ids.join(","),
+        'type' : 'hidden'
+      }));
+      form.append();
+      form.submit();
+      form.remove();
+  }
+  
+  function addClickToDownloadReady() {
+    original_download_ready_URL = $('#download-ready').attr('href');
+    $('#download-ready').click(function(){modifyActionButtonURL('#download-ready', original_download_ready_URL);});
+  }
+  function addClickToUploadButton() {
+    original_upload_edited_URL = $('#upload-edited').attr('href');
+    $('#upload-edited').click(function(){modifyActionButtonURL('#upload-edited', original_upload_edited_URL);});
+  }
+  //changes the href associated with the download/upload buttons after they are clicked
+  //but before the links are actually followed. Also checks to see if the results are 
+  //filtered.
+  function modifyActionButtonURL(element_id, original_URL) {
+    var new_URL = original_URL.valueOf();//clones the original
+    var entity_ids = getIDArray();
+    var filterValue = $('#clear-filters').text();
+    console.log(filterValue);
+    var filterOn = filterValue !== "";
+    if(filterOn === true && entity_ids.length === 0) {
+      scrapeAllFilteredPages(element_id, original_URL);
+    }
+    else {
+      var id_string = entity_ids.join(",");
+      new_URL += entity_ids.length !== 0 ? "/" + entity_ids.join(",") : "";
+      new_URL = entity_ids.length === 0 ? original_URL : new_URL;
+      $(element_id).attr('href', new_URL);
+    }
+  }
+  //looks at every currently displayed row and pushes the entity_id of each
+  //row with a checked checkbox into the return variable
+  function getIDArray(visible_check) {
+    var entity_ids = [];
+    var visible = visible_check === true;
+    $('#edit-grid-container .form-checkbox').each(function() {
+        var val = $(this).val();
+        if($(this).attr('checked') || visible) {
+          if(val !== 'on') {//'on' represents the 'select all' checkbox
+            entity_ids.push(val);
+          }
+        }
+    });
+    return entity_ids;
+  }
 })(jQuery);
