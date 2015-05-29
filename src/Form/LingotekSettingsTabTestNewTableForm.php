593aab3157ff41cc8304a771680dc6efa40a19b7
Joseph Hovik <jhovik@lingotek.com>
2015-05-29 10:56:15 -0600

    Issue by jbhovik: Template of Translate Content table working

--- src/Form/LingotekSettingsTabTestNewTableForm.php
+++ src/Form/LingotekSettingsTabTestNewTableForm.php
@@ -28,100 +28,47 @@ class LingotekSettingsTabTestNewTableForm extends LingotekConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
+    $profiles = $this->L->get('profile');
+    $pullDownProfiles = array();
+    foreach($profiles as $profile){
+      $pullDownProfiles[] = $profile['name'];
+    }
     $menuItems = array('Good', 'Bad', 'Ugly');
+    $entities = array('block_content', 'block', 'page');
+    $fields = array('Body', 'Title');
 
     $header = array(
-      t('Checkbox'),
-      t('Select'),
-      t('Radio'),
+      t('Entity'),
+      t('Profile'),
+      t('Fields'),
     );
-
-    $rows = array(
-      array(
-        t('1'),
-        t('2'),
-        t('3'),
-      ),
-      array(
-        t('4'),
-        t('5'),
-        t('6'),
-      ),
-      array(
-        t('7'),
-        t('8'),
-        t('9'),
-      ),
-    );
-
-    $test = array('#type' => 'select');
-
-    $testCheckbox1 = array(
-      '#type' => 'checkbox',
-      '#title' => t('Checkbox1'),
-    );
-
-    $testSelect1 = array(
-      '#type' => 'select',
-      '#title' => t('Select1'),
-      '#options' => $menuItems,
-    );
-
-    $testRadio1 = array(
-      '#type' => 'radios',
-      '#options' => array('Radio1'),
-    );
-
-    $row1 = array($testCheckbox1, $testSelect1, $testRadio1);
-
-    $testCheckbox2 = array(
-      '#type' => 'checkbox',
-      '#title' => t('Checkbox2'),
-    );
-
-    $testSelect2 = array(
-      '#type' => 'select',
-      '#title' => t('Select2'),
-      '#options' => $menuItems,
-    );
-
-     $testRadio2 = array(
-      '#type' => 'radios',
-      '#options' => array('Radio2'),
-    );
-
-    $row2 = array($testCheckbox2, $testSelect2, $testRadio2);
-
-    $testCheckbox3 = array(
-      '#type' => 'checkbox',
-      '#title' => t('Checkbox3'),
-    );
-
-    $testSelect3 = array(
-      '#type' => 'select',
-      '#title' => t('Select3'),
-      '#options' => $menuItems,
-    );
-
-     $testRadio3 = array(
-      '#type' => 'radios',
-      '#options' => array('Radio3'),
-    );
-
-    $row3 = array($testCheckbox3, $testSelect3, $testRadio3);
-
     $table = array(
       '#type' => 'table',
       '#header' => $header,
       '#empty' => t('No Entries'),
     );
 
-    $table['row1'] = $row1;
-    $table['row2'] = $row2;
-    $table['row3'] = $row3;
-
-    
+    foreach ($entities as $entity) {
+      $row = array();
+      $name = array('#markup' => $entity);
+      $row[] = $name;
+      $select = array(
+        '#type' => 'select',
+        '#options' => $pullDownProfiles,
+      );
+      $row[] = $select;
+      $checkboxRow = array();
+      
+      foreach ($fields as $field) {
+        $checkbox = array(
+          '#type' => 'checkbox',
+          '#title' => $field,
+        );
+        $checkboxRow[] = $checkbox;
+      }
+      $row[] = $checkboxRow;
+      $table[$entity] = $row;
+    }
 
     $form['table'] = $table;
 
