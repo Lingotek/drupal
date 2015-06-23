78df2f1e3a9e49d98ae196868a32972e0f1888bb
Joseph Hovik <jhovik@lingotek.com>
2015-06-23 10:25:45 -0600

    Issue by jbhovik: Added Multilingual Preparation Utilities skeleton code

--- src/Form/LingotekSettingsTabUtilitiesForm.php
+++ src/Form/LingotekSettingsTabUtilitiesForm.php
@@ -30,7 +30,81 @@ class LingotekSettingsTabUtilitiesForm extends LingotekConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $table = array(
+    $form['utilities'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Utilities'),
+    );
+
+    $form['utilities']['multilingual_title'] = array(
+      '#markup' => '<H4>' . $this->t('Multilingual Preparation Utilities' . '</H4>'),
+    );
+
+    $cleanup_function_headers = array(
+      'title' => array('data' => t('Utility')),
+      'desc' => array('data' => t('Description')),
+    );
+
+    $cleanup_functions = array();
+
+    $cleanup_functions['lingotek_batch_identify_translations'] = array(
+      'title' => t('<span style="white-space: nowrap">Identify existing translations</span>'),
+      'desc' => t('Identifies existing node translations currently untracked by the Lingotek module. The translation status for all newly discovered translations will be set to <i>current</i>.'),
+    );
+
+    $cleanup_functions['lingotek_cleanup_field_languages_for_nodes'] = array(
+      'title' => t('Prepare nodes'),
+      'desc' => t('Sets all <i>language neutral</i> nodes (and underlying fields and path aliases) to be @lang.', array('@lang' => \Drupal::languageManager()->getDefaultLanguage()->getName())),
+    );
+
+    $cleanup_functions['lingotek_cleanup_notify_entity_translation'] = array(
+      'title' => t('Sync with Entity Translation'),
+      'desc' => t('Reports all translations managed by Lingotek to the Entity Translation module.'),
+    );
+
+    $cleanup_functions['lingotek_cleanup_field_languages_for_comments'] = array(
+      'title' => t('Prepare comments'),
+      'desc' => t('Sets all <i>language neutral</i> comments (and underlying fields) to be @lang.', array('@lang' => \Drupal::languageManager()->getDefaultLanguage()->getName())),
+    );
+
+    $cleanup_functions['lingotek_cleanup_field_languages_for_taxonomy_terms'] = array(
+      'title' => t('Prepare taxonomy terms with custom fields'),
+      'desc' => t('Sets all <i>language neutral</i> taxonomy terms managed by Lingotek (and underlying fields) to be @lang.', array('@lang' => \Drupal::languageManager()->getDefaultLanguage()->getName())),
+    );
+
+    $cleanup_functions['lingotek_admin_prepare_blocks'] = array(
+      'title' => t('Prepare blocks'),
+      'desc' => t('Update all blocks to be translatable in the <i>Languages</i> settings.'),
+    );
+
+    $cleanup_functions['lingotek_admin_prepare_taxonomies'] = array(
+      'title' => t('Prepare taxonomy'),
+      'desc' => t('Update all taxonomy vocabularies that are not currently enabled for multilingual to use translation mode <i>Localize</i> in the Multilingual Options. (Note: Translation mode <i>Localize</i> does not support translation of custom fields within taxonomy terms.)'),
+    );
+
+    $cleanup_functions['lingotek_admin_prepare_menus'] = array(
+      'title' => t('Prepare menus'),
+      'desc' => t('Update all menus to use <i>Translate and Localize</i> in the Multilingual Options, and update all menu links to be <i>Language neutral</i>.'),
+    );
+
+    $cleanup_functions['lingotek_add_missing_locales'] = array(
+      'title' => t('Add missing locales'),
+      'desc' => t('Fills in any missing locale values to the <i>languages</i> table.'),
+    );
+
+    $form['utilities']['grid'] = array(
+      '#type' => 'tableselect',
+      '#header' => $cleanup_function_headers,
+      '#options' => $cleanup_functions,
+    );
+
+    $form['utilities']['actions']['run_button'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Run selected utilities'),
+      '#button_type' => 'primary',
+      '#submit' => array('::runSelectedUtilities'),
+    );
+
+    $lingotek_table = array(
       '#type' => 'table',
       '#empty' => $this->t('No Entries'),
     );
@@ -59,17 +133,13 @@ class LingotekSettingsTabUtilitiesForm extends LingotekConfigFormBase {
       '#submit' => array('::disassociateAllTranslations'),
     );
 
-    $table['api_refresh'] = $api_refresh_row;
-    $table['disassociate'] = $disassociate_row;
-    $form['utilities'] = array(
-      '#type' => 'details',
-      '#title' => $this->t('Utilities'),
-    );
-
+    $lingotek_table['api_refresh'] = $api_refresh_row;
+    $lingotek_table['disassociate'] = $disassociate_row;
+    
     $form['utilities']['utilities_title'] = array(
-      '#markup' => '<H4>' . $this->t('Lingotek Utilities' . '</H4>'),
+      '#markup' => '<br><br><H4>' . $this->t('Lingotek Utilities' . '</H4>'),
     );
-    $form['utilities']['table'] = $table;
+    $form['utilities']['lingotek_table'] = $lingotek_table;
 
     return $form;
   }
@@ -82,7 +152,12 @@ class LingotekSettingsTabUtilitiesForm extends LingotekConfigFormBase {
 
   public function disassociateAllTranslations() {
     LingotekSync::disassociateAllEntities();
+    LingotekSync::disassociateAllSets();
     drupal_set_message($this->t('All translations have been disassociated.'));
+  }
+
+  public function runSelectedUtilities() {
+    
   }
 
 }
