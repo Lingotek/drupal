6b81f3e185226e859bbaccee9c47bb8ae54e9340
Christian Lopez Espinola <penyaskito@computer.org>
2015-10-06 15:39:36 +0200

    New :variable placeholder for URLs in HTML attributes added to SafeMarkup::format() and t() (https://www.drupal.org/node/2571689)

--- src/Controller/LingotekManagementController.php
+++ src/Controller/LingotekManagementController.php
@@ -25,11 +25,11 @@ class LingotekManagementController extends LingotekControllerBase {
     }
 
     $build['enable_content_translation']['#markup'] =
-      $this->t('You need to enable content translation first. You can enable translation for the desired content entities on the <a href="!translation-entity">Content language</a> page.',
-        ['!translation-entity' => \Drupal::url('language.content_settings_page')]) . '<br/>';
+      $this->t('You need to enable content translation first. You can enable translation for the desired content entities on the <a href=":translation-entity">Content language</a> page.',
+        [':translation-entity' => \Drupal::url('language.content_settings_page')]) . '<br/>';
     $build['enable_lingotek']['#markup'] =
-      $this->t('Then you need to configure how you want to translate your content with Lingotek. Enable translation for the desired content entities on the <a href="!lingotek-translation-entity">Lingotek settings</a> page.',
-        ['!lingotek-translation-entity' => \Drupal::url('lingotek.settings')]);
+      $this->t('Then you need to configure how you want to translate your content with Lingotek. Enable translation for the desired content entities on the <a href=":lingotek-translation-entity">Lingotek settings</a> page.',
+        [':lingotek-translation-entity' => \Drupal::url('lingotek.settings')]);
 
     return $build;
   }
