5f58d82dfeeddb22015292365621f3cdc03f7f6a
Matt Smith <msmith@lingotek.com>
2015-11-16 13:10:12 -0700

    Update to connect.js to pass along entire hash (including additional params)

--- js/connect.js
+++ js/connect.js
@@ -1,12 +1,7 @@
-/**
- * Created by rob on 8/22/14.
- */
 
   var hash = window.location.hash;
   if (hash.length && hash.indexOf("access_token") !== -1) {
-    var parts = hash.substring(1).split('=');
-    var access_token = parts[1];
-    var url_with_access_token = window.location.origin + window.location.pathname + window.location.search + '&access_token=' + access_token;
+    var url_with_access_token = window.location.origin + window.location.pathname + window.location.search + '&' + hash.substr(1);
     window.location.href = url_with_access_token;
   } else {
     window.location.href = window.location.origin + window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
