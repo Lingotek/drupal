af0a143828fbb5abcac99fbfad59bdf26791f1cf
penyaskito <penyaskito@959536.no-reply.drupal.org>
2017-03-10 16:37:56 +0100

    Issue #2859616 by penyaskito: Add tests for Filtering by title in the content management bulk form

--- src/Tests/Form/LingotekNodeBulkFormTest.php
+++ src/Tests/Form/LingotekNodeBulkFormTest.php
@@ -184,4 +184,85 @@ class LingotekNodeBulkFormTest extends LingotekTestBase {
     $this->assertLinkByHref('?page=1');
   }
 
+  /**
+   * Tests that the bulk management label filtering works correctly.
+   */
+  public function testLabelFilter() {
+    $nodes = [];
+    // Create a node.
+    for ($i = 1; $i < 15; $i++) {
+      $animal = 'Llamas';
+      if ($i % 2 == 0) {
+        $animal = 'Dogs';
+      }
+      elseif ($i % 3 == 0) {
+        $animal = 'Cats';
+      }
+
+      $edit = array();
+      $edit['title[0][value]'] = $animal . ' are cool ' . $i;
+      $edit['body[0][value]'] = $animal . ' are very cool ' . $i;
+      $edit['langcode[0][value]'] = 'en';
+      $edit['lingotek_translation_profile'] = 'manual';
+      $this->drupalPostForm('node/add/article', $edit, t('Save and publish'));
+      $nodes[$i] = $edit;
+    }
+
+    $this->goToContentBulkManagementForm();
+
+    // Assert there is a pager.
+    $this->assertLinkByHref('?page=1');
+
+    // After we filter by label 'Llamas', there is no pager and the rows
+    // selected are the ones expected.
+    $edit = [
+      'filters[wrapper][label]' => 'Llamas',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Filter');
+    foreach ([1, 5, 7, 11, 13] as $j) {
+      $this->assertLink('Llamas are cool ' . $j);
+    }
+    $this->assertNoLinkByHref('?page=1');
+    $this->assertNoLink('Dogs are cool 2');
+
+    // After we filter by label 'Dogs', there is no pager and the rows selected
+    // are the ones expected.
+    $edit = [
+      'filters[wrapper][label]' => 'Dogs',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Filter');
+    foreach ([2, 4, 6, 8, 10, 12, 14] as $j) {
+      $this->assertLink('Dogs are cool ' . $j);
+    }
+    $this->assertNoLink('Page 2');
+    $this->assertNoLink('Llamas are cool 1');
+
+    // After we filter by label 'Cats', there is no pager and the rows selected
+    // are the ones expected.
+    $edit = [
+      'filters[wrapper][label]' => 'Cats',
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Filter');
+    foreach ([3, 9] as $j) {
+      $this->assertLink('Cats are cool ' . $j);
+    }
+    $this->assertNoLinkByHref('?page=1');
+    $this->assertNoLink('Dogs are cool 5');
+
+    // After we reset, we get back to having a pager and all the content under
+    // limit of 10.
+    $this->drupalPostForm(NULL, [], 'Reset');
+    foreach ([1, 5, 7] as $j) {
+      $this->assertLink('Llamas are cool ' . $j);
+    }
+    foreach ([2, 4, 6, 8, 10] as $j) {
+      $this->assertLink('Dogs are cool ' . $j);
+    }
+    foreach ([3, 9] as $j) {
+      $this->assertLink('Cats are cool ' . $j);
+    }
+
+    $this->assertLinkByHref('?page=1');
+  }
+
 }
