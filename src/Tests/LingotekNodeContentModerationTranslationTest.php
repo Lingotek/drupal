5be49b67d62244b03874f3dfc1da33d6d39b625a
penyaskito <penyaskito@959536.no-reply.drupal.org>
2017-07-20 22:05:41 +0200

    Issue #2830119 by penyaskito, t.murphy, tbfisher: Add tests ensuring entities with moderation enabled don't create multiple revisions with Lingotek enabled

---
+++ src/Tests/LingotekNodeContentModerationTranslationTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Drupal\lingotek\Tests;
+
+use Drupal\language\Entity\ConfigurableLanguage;
+use Drupal\language\Entity\ContentLanguageSettings;
+
+/**
+ * Tests translating a node with content moderation enabled.
+ *
+ * @group lingotek
+ */
+class LingotekNodeContentModerationTranslationTest extends LingotekTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['block', 'node', 'content_moderation'];
+
+  protected function setUp() {
+    parent::setUp();
+
+    // Place the actions and title block.
+    $this->drupalPlaceBlock('page_title_block', ['region' => 'content', 'weight' => -5]);
+    $this->drupalPlaceBlock('local_tasks_block', ['region' => 'content', 'weight' => -10]);
+
+    // Create Article node types.
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+
+    // Add a language.
+    ConfigurableLanguage::createFromLangcode('es')->setThirdPartySetting('lingotek', 'locale', 'es_MX')->save();
+
+    // Enable translation for the current entity type and ensure the change is
+    // picked up.
+    ContentLanguageSettings::loadByEntityTypeBundle('node', 'article')->setLanguageAlterable(TRUE)->save();
+    \Drupal::service('content_translation.manager')->setEnabled('node', 'article', TRUE);
+
+    drupal_static_reset();
+    \Drupal::entityManager()->clearCachedDefinitions();
+    \Drupal::service('entity.definition_update_manager')->applyUpdates();
+    // Rebuild the container so that the new languages are picked up by services
+    // that hold a list of languages.
+    $this->rebuildContainer();
+
+    $edit = [
+      'node[article][enabled]' => 1,
+      'node[article][profiles]' => 'automatic',
+      'node[article][fields][title]' => 1,
+      'node[article][fields][body]' => 1,
+    ];
+    $this->drupalPostForm('admin/lingotek/settings', $edit, 'Save', [], [], 'lingoteksettings-tab-content-form');
+
+  }
+
+  /**
+   * Tests that no new revisions are created when processing with Lingotek.
+   */
+  public function testNoExtraRevisionsCreatedWhenProcessing() {
+    // Login as admin.
+    $this->drupalLogin($this->rootUser);
+
+    // Enable content moderation for articles.
+    $this->drupalPostForm('/admin/structure/types/manage/article/moderation', ['workflow' => 'editorial'], t('Save'));
+
+    // Create a node.
+    $edit = [];
+    $edit['title[0][value]'] = 'Llamas are cool';
+    $edit['body[0][value]'] = 'Llamas are very cool';
+    $edit['langcode[0][value]'] = 'en';
+    $edit['lingotek_translation_profile'] = 'manual';
+    $this->drupalPostForm('node/add/article', $edit, t('Save and Publish'));
+
+    $this->goToContentBulkManagementForm();
+
+    $basepath = \Drupal::request()->getBasePath();
+
+    // Clicking English must init the upload of content.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/upload/node/1?destination=' . $basepath . '/admin/lingotek/manage/node');
+    // And we cannot request yet a translation.
+    $this->assertNoLinkByHref($basepath . '/admin/lingotek/entity/add_target/dummy-document-hash-id/es_MX?destination=' . $basepath . '/admin/lingotek/manage/node');
+    $this->clickLink('EN');
+    $this->assertText('Node Llamas are cool has been uploaded.');
+    $this->assertIdentical('en_US', \Drupal::state()->get('lingotek.uploaded_locale'));
+
+    // There is a link for checking status.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/check_upload/dummy-document-hash-id?destination=' . $basepath . '/admin/lingotek/manage/node');
+    // And we can already request a translation.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/add_target/dummy-document-hash-id/es_MX?destination=' . $basepath . '/admin/lingotek/manage/node');
+    $this->clickLink('EN');
+    $this->assertText('The import for node Llamas are cool is complete.');
+
+    // Request the Spanish translation.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/add_target/dummy-document-hash-id/es_MX?destination=' . $basepath . '/admin/lingotek/manage/node');
+    $this->clickLink('ES');
+    $this->assertText("Locale 'es_MX' was added as a translation target for node Llamas are cool.");
+    $this->assertIdentical('es_MX', \Drupal::state()->get('lingotek.added_target_locale'));
+
+    // Check status of the Spanish translation.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/check_target/dummy-document-hash-id/es_MX?destination=' . $basepath . '/admin/lingotek/manage/node');
+    $this->clickLink('ES');
+    $this->assertIdentical('es_MX', \Drupal::state()->get('lingotek.checked_target_locale'));
+    $this->assertText('The es_MX translation for node Llamas are cool is ready for download.');
+
+    // Download the Spanish translation.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/entity/download/dummy-document-hash-id/es_MX?destination=' . $basepath . '/admin/lingotek/manage/node');
+    $this->clickLink('ES');
+    $this->assertText('The translation of node Llamas are cool into es_MX has been downloaded.');
+    $this->assertIdentical('es_MX', \Drupal::state()->get('lingotek.downloaded_locale'));
+
+    // Now the link is to the workbench, and it opens in a new tab.
+    $this->assertLinkByHref($basepath . '/admin/lingotek/workbench/dummy-document-hash-id/es_MX');
+    $workbench_link = $this->xpath("//a[@href='$basepath/admin/lingotek/workbench/dummy-document-hash-id/es_MX' and @target='_blank']");
+    $this->assertEqual(count($workbench_link), 1, 'Workbench links open in a new tab.');
+
+    // Open the node.
+    $this->clickLink('Llamas are cool');
+
+    // There is no revisions tab as it's the only one revision.
+    $this->assertNoLink('Revisions', 'No revisions tab shown as there is only one revision.');
+
+    // Only one revision stored.
+    /** @var \Drupal\node\NodeStorageInterface $node_storage */
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $result = $node_storage->getQuery()
+      ->allRevisions()
+      ->condition('nid', 1)
+      ->sort('vid', 'DESC')
+      ->pager(50)
+      ->count()
+      ->execute();
+    $this->assertEqual(1, $result, 'Only one revision is stored.');
+  }
+
+}
