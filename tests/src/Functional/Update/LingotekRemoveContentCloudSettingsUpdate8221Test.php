4ac1c088bd7d94af36a67e2e6459790f5f741e01
penyaskito <penyaskito@959536.no-reply.drupal.org>
2020-04-19 12:37:07 +0200

    Issue #3128752 by penyaskito: Remove Lingotek Content Cloud

---
+++ tests/src/Functional/Update/LingotekRemoveContentCloudSettingsUpdate8221Test.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\lingotek\Functional\Update;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+
+/**
+ * Tests the upgrade path for removing Lingotek Content Cloud related preferences.
+ *
+ * @group lingotek
+ * @group legacy
+ */
+class LingotekRemoveContentCloudSettingsUpdate8221Test extends UpdatePathTestBase {
+
+  /**
+   * The Lingotek configuration service.
+   *
+   * @var \Drupal\lingotek\LingotekConfigurationServiceInterface
+   */
+  protected $lingotekConfiguration;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->lingotekConfiguration = $this->container->get('lingotek.configuration');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles() {
+    $this->databaseDumpFiles = [
+      __DIR__ . '/../../../fixtures/update/drupal-8.lingotek.standard.pre8217.php.gz',
+    ];
+  }
+
+  /**
+   * Tests that the upgrade removes Lingotek Content Cloud related preferences.
+   */
+  public function testUpgrade() {
+    $this->assertFalse($this->lingotekConfiguration->getPreference('enable_content_cloud'));
+    $this->assertEqual($this->lingotekConfiguration->getPreference('content_cloud_import_format'), 'article');
+    $this->assertEqual($this->lingotekConfiguration->getPreference('content_cloud_import_status'), 0);
+
+    $this->runUpdates();
+
+    $this->assertNull($this->lingotekConfiguration->getPreference('enable_content_cloud'));
+    $this->assertNull($this->lingotekConfiguration->getPreference('content_cloud_import_format'));
+    $this->assertNull($this->lingotekConfiguration->getPreference('content_cloud_import_status'));
+  }
+
+}
